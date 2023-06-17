806c98ef7a3a1164d8ef11f9bb2b51339e960bee
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-06-17 00:48:30 +0000

    completed project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our", "his", "her", "their"];
+  let adj = ["great", "blue", "neon", "holy", "huge"];
+  let noun = ["temple", "car", "school", "house", "train"];
+  let dots = [".com", ".io", ".us", ".net", ".gov"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < dots.length; d++) {
+          let output = `${pronoun[a]}${adj[b]}${noun[c]}${dots[d]}`;
+          console.log(output);
+        }
+      }
+    }
+  }
 };
